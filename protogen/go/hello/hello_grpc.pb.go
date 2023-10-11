b30e9aca9a8a5b5450198ac011b34fed538afac9
dwprz <dwprz@users.noreply.github.com>
2023-10-11 00:55:08 +0000

    chore(protobuf-go) : Auto-generated by Github Action

--- protogen/go/hello/hello_grpc.pb.go
+++ protogen/go/hello/hello_grpc.pb.go
@@ -19,8 +19,10 @@ import (
 const _ = grpc.SupportPackageIsVersion7
 
 const (
-	HelloService_SayHello_FullMethodName      = "/HelloService/SayHello"
-	HelloService_SayManyHellos_FullMethodName = "/HelloService/SayManyHellos"
+	HelloService_SayHello_FullMethodName           = "/HelloService/SayHello"
+	HelloService_SayManyHellos_FullMethodName      = "/HelloService/SayManyHellos"
+	HelloService_SayHelloToEveryone_FullMethodName = "/HelloService/SayHelloToEveryone"
+	HelloService_SayHelloContinuous_FullMethodName = "/HelloService/SayHelloContinuous"
 )
 
 // HelloServiceClient is the client API for HelloService service.
@@ -29,6 +31,8 @@ const (
 type HelloServiceClient interface {
 	SayHello(ctx context.Context, in *HelloRequest, opts ...grpc.CallOption) (*HelloResponse, error)
 	SayManyHellos(ctx context.Context, in *HelloRequest, opts ...grpc.CallOption) (HelloService_SayManyHellosClient, error)
+	SayHelloToEveryone(ctx context.Context, opts ...grpc.CallOption) (HelloService_SayHelloToEveryoneClient, error)
+	SayHelloContinuous(ctx context.Context, opts ...grpc.CallOption) (HelloService_SayHelloContinuousClient, error)
 }
 
 type helloServiceClient struct {
@@ -80,12 +84,79 @@ func (x *helloServiceSayManyHellosClient) Recv() (*HelloResponse, error) {
 	return m, nil
 }
 
+func (c *helloServiceClient) SayHelloToEveryone(ctx context.Context, opts ...grpc.CallOption) (HelloService_SayHelloToEveryoneClient, error) {
+	stream, err := c.cc.NewStream(ctx, &HelloService_ServiceDesc.Streams[1], HelloService_SayHelloToEveryone_FullMethodName, opts...)
+	if err != nil {
+		return nil, err
+	}
+	x := &helloServiceSayHelloToEveryoneClient{stream}
+	return x, nil
+}
+
+type HelloService_SayHelloToEveryoneClient interface {
+	Send(*HelloRequest) error
+	CloseAndRecv() (*HelloResponse, error)
+	grpc.ClientStream
+}
+
+type helloServiceSayHelloToEveryoneClient struct {
+	grpc.ClientStream
+}
+
+func (x *helloServiceSayHelloToEveryoneClient) Send(m *HelloRequest) error {
+	return x.ClientStream.SendMsg(m)
+}
+
+func (x *helloServiceSayHelloToEveryoneClient) CloseAndRecv() (*HelloResponse, error) {
+	if err := x.ClientStream.CloseSend(); err != nil {
+		return nil, err
+	}
+	m := new(HelloResponse)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+func (c *helloServiceClient) SayHelloContinuous(ctx context.Context, opts ...grpc.CallOption) (HelloService_SayHelloContinuousClient, error) {
+	stream, err := c.cc.NewStream(ctx, &HelloService_ServiceDesc.Streams[2], HelloService_SayHelloContinuous_FullMethodName, opts...)
+	if err != nil {
+		return nil, err
+	}
+	x := &helloServiceSayHelloContinuousClient{stream}
+	return x, nil
+}
+
+type HelloService_SayHelloContinuousClient interface {
+	Send(*HelloRequest) error
+	Recv() (*HelloResponse, error)
+	grpc.ClientStream
+}
+
+type helloServiceSayHelloContinuousClient struct {
+	grpc.ClientStream
+}
+
+func (x *helloServiceSayHelloContinuousClient) Send(m *HelloRequest) error {
+	return x.ClientStream.SendMsg(m)
+}
+
+func (x *helloServiceSayHelloContinuousClient) Recv() (*HelloResponse, error) {
+	m := new(HelloResponse)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
 // HelloServiceServer is the server API for HelloService service.
 // All implementations must embed UnimplementedHelloServiceServer
 // for forward compatibility
 type HelloServiceServer interface {
 	SayHello(context.Context, *HelloRequest) (*HelloResponse, error)
 	SayManyHellos(*HelloRequest, HelloService_SayManyHellosServer) error
+	SayHelloToEveryone(HelloService_SayHelloToEveryoneServer) error
+	SayHelloContinuous(HelloService_SayHelloContinuousServer) error
 	mustEmbedUnimplementedHelloServiceServer()
 }
 
@@ -98,6 +169,12 @@ func (UnimplementedHelloServiceServer) SayHello(context.Context, *HelloRequest) 
 }
 func (UnimplementedHelloServiceServer) SayManyHellos(*HelloRequest, HelloService_SayManyHellosServer) error {
 	return status.Errorf(codes.Unimplemented, "method SayManyHellos not implemented")
+}
+func (UnimplementedHelloServiceServer) SayHelloToEveryone(HelloService_SayHelloToEveryoneServer) error {
+	return status.Errorf(codes.Unimplemented, "method SayHelloToEveryone not implemented")
+}
+func (UnimplementedHelloServiceServer) SayHelloContinuous(HelloService_SayHelloContinuousServer) error {
+	return status.Errorf(codes.Unimplemented, "method SayHelloContinuous not implemented")
 }
 func (UnimplementedHelloServiceServer) mustEmbedUnimplementedHelloServiceServer() {}
 
@@ -151,6 +228,58 @@ func (x *helloServiceSayManyHellosServer) Send(m *HelloResponse) error {
 	return x.ServerStream.SendMsg(m)
 }
 
+func _HelloService_SayHelloToEveryone_Handler(srv interface{}, stream grpc.ServerStream) error {
+	return srv.(HelloServiceServer).SayHelloToEveryone(&helloServiceSayHelloToEveryoneServer{stream})
+}
+
+type HelloService_SayHelloToEveryoneServer interface {
+	SendAndClose(*HelloResponse) error
+	Recv() (*HelloRequest, error)
+	grpc.ServerStream
+}
+
+type helloServiceSayHelloToEveryoneServer struct {
+	grpc.ServerStream
+}
+
+func (x *helloServiceSayHelloToEveryoneServer) SendAndClose(m *HelloResponse) error {
+	return x.ServerStream.SendMsg(m)
+}
+
+func (x *helloServiceSayHelloToEveryoneServer) Recv() (*HelloRequest, error) {
+	m := new(HelloRequest)
+	if err := x.ServerStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+func _HelloService_SayHelloContinuous_Handler(srv interface{}, stream grpc.ServerStream) error {
+	return srv.(HelloServiceServer).SayHelloContinuous(&helloServiceSayHelloContinuousServer{stream})
+}
+
+type HelloService_SayHelloContinuousServer interface {
+	Send(*HelloResponse) error
+	Recv() (*HelloRequest, error)
+	grpc.ServerStream
+}
+
+type helloServiceSayHelloContinuousServer struct {
+	grpc.ServerStream
+}
+
+func (x *helloServiceSayHelloContinuousServer) Send(m *HelloResponse) error {
+	return x.ServerStream.SendMsg(m)
+}
+
+func (x *helloServiceSayHelloContinuousServer) Recv() (*HelloRequest, error) {
+	m := new(HelloRequest)
+	if err := x.ServerStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
 // HelloService_ServiceDesc is the grpc.ServiceDesc for HelloService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -168,6 +297,17 @@ var HelloService_ServiceDesc = grpc.ServiceDesc{
 			StreamName:    "SayManyHellos",
 			Handler:       _HelloService_SayManyHellos_Handler,
 			ServerStreams: true,
+		},
+		{
+			StreamName:    "SayHelloToEveryone",
+			Handler:       _HelloService_SayHelloToEveryone_Handler,
+			ClientStreams: true,
+		},
+		{
+			StreamName:    "SayHelloContinuous",
+			Handler:       _HelloService_SayHelloContinuous_Handler,
+			ServerStreams: true,
+			ClientStreams: true,
 		},
 	},
 	Metadata: "proto/hello/hello.proto",
